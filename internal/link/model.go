6bec305bb733474f2c1fb0df00b971bb424374cb
akazezov <kazezov.a@rahmetapp.kz>
2024-12-15 14:59:43 +0500

    feat(link): link model

---
+++ internal/link/model.go
@@ -0,0 +1,30 @@
+package link
+
+import (
+	"math/rand"
+
+	"gorm.io/gorm"
+)
+
+type Link struct {
+	gorm.Model
+	Url  string `json:"url"`
+	Hash string `json:"hash" gorm:"uniqueIndex"`
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func NewLetter(url string) *Link {
+	return &Link{
+		Url:  url,
+		Hash: randStringRunes(6),
+	}
+}
+
+func randStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
