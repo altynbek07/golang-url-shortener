9f96ed32976b9edf0c4b0a1e46d918fb789e73f5
akazezov <kazezov.a@rahmetapp.kz>
2024-12-15 15:08:53 +0500

    feat(db): auto migrate

--- configs/config.go
+++ configs/config.go
@@ -24,6 +24,7 @@ func LoadConfig() *Config {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+		panic(err)
 	}
 
 	return &Config{
---
+++ migrations/auto.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"go/adv-demo/internal/link"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		panic(err)
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&link.Link{})
+}
