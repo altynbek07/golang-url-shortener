356aa53168164e555faa9adea24c26226352da3a
akazezov <kazezov.a@rahmetapp.kz>
2024-12-25 19:05:53 +0500

    feat(auth): enhance login tests with database setup and teardown

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -4,13 +4,53 @@ import (
 	"bytes"
 	"encoding/json"
 	"go/adv-demo/internal/auth"
+	"go/adv-demo/internal/user"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+func initDb() *gorm.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		panic(err)
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func initData(db *gorm.DB) {
+	db.Create(&user.User{
+		Email:    "altynbek2@altynbek.com",
+		Password: "$2a$10$jSZ5wo4.ogX3PyNVFJnu7uoWe7IZVzQjjuQTi0C61HwtR3iwdJAZy", // 1234
+		Name:     "Altynbek",
+	})
+}
+
+func removeData(db *gorm.DB) {
+	db.Unscoped().
+		Where("email = ?", "altynbek2@altynbek.com").
+		Delete(&user.User{})
+}
+
 func TestLoginSuccess(t *testing.T) {
+	// Prepare
+	db := initDb()
+	initData(db)
+
 	ts := httptest.NewServer(App())
 	defer ts.Close()
 
@@ -42,9 +82,15 @@ func TestLoginSuccess(t *testing.T) {
 	if resData.Token == "" {
 		t.Fatal("Token is empty")
 	}
+
+	removeData(db)
 }
 
 func TestLoginFail(t *testing.T) {
+	// Prepare
+	db := initDb()
+	initData(db)
+
 	ts := httptest.NewServer(App())
 	defer ts.Close()
 
@@ -61,4 +107,6 @@ func TestLoginFail(t *testing.T) {
 	if res.StatusCode != http.StatusUnauthorized {
 		t.Fatalf("Expected status code %d, but got %d", http.StatusUnauthorized, res.StatusCode)
 	}
+
+	removeData(db)
 }
